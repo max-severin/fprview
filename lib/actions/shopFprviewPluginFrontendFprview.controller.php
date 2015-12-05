2ee124647c3e11c457ba76a297316345236dd32a
Max Severin <makc.severin@gmail.com>
2015-12-05 23:30:15 +0300

    created plugin basic structure

---
+++ lib/actions/shopFprviewPluginFrontendFprview.controller.php
@@ -0,0 +1,29 @@
+<?php
+
+class shopPmoreinfoPluginFrontendPmoreinfoController extends waJsonController {
+
+    public function execute() {
+        $id = (int)waRequest::get('id');      
+        
+        $product = new shopProduct($id);
+        $route_params = array('product_url' => $product['url']);
+        if (isset($product['category_url'])) {
+            $route_params['category_url'] = $product['category_url'];
+        }
+
+        $product['frontend_url'] = wa()->getRouteUrl('shop/frontend/product', $route_params);
+
+        $feature_codes = array_keys($product->features);
+        $feature_model = new shopFeatureModel();
+        $features = $feature_model->getByCode($feature_codes);
+
+        $view = wa()->getView();
+        $view->assign('features', $features);
+        $view->assign('product', $product);
+        $html = $view->fetch(realpath(dirname(__FILE__)."/../../").'/templates/Frontend.html');
+
+        echo $html;
+        exit();
+    }  
+     
+}
---
+++ lib/config/plugin.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'name' => 'Быстрый просмотр товара',
+    'version' => '1.0.0',
+    'frontend' => true,
+    'handlers' => array(
+    	//
+    ),
+);
