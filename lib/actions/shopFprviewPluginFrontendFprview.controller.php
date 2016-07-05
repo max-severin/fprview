cb73329d05ff111767deed45a0afc84f5d10a14b
Max Severin <makc.severin@gmail.com>
2016-07-05 04:47:38 +0300

    work on template type setting

--- lib/actions/shopFprviewPluginFrontendFprview.controller.php
+++ lib/actions/shopFprviewPluginFrontendFprview.controller.php
@@ -1,9 +1,12 @@
 <?php
 
-class shopFprviewPluginFrontendFprviewController extends waJsonController {
+class shopFprviewPluginFrontendFprviewController extends shopFrontendAction {
 
     public function execute() {
-        $id = (int)waRequest::get('id');      
+        $id = (int)waRequest::get('id');    
+
+        $app_settings_model = new waAppSettingsModel();
+        $settings = $app_settings_model->get(array('shop', 'fprview'));  
         
         $product = new shopProduct($id);
         $route_params = array('product_url' => $product['url']);
@@ -20,10 +23,53 @@ class shopFprviewPluginFrontendFprviewController extends waJsonController {
         $view = wa()->getView();
         $view->assign('features', $features);
         $view->assign('product', $product);
-        $html = $view->fetch(realpath(dirname(__FILE__)."/../../").'/templates/Frontend.html');
+
+        switch ($settings['template_type']) {
+            case 'plugin':
+                $html = $view->fetch(realpath(dirname(__FILE__)."/../../").'/templates/Frontend.html');
+                break;
+            case 'theme':
+                $html = $this->renderTemplate(array(
+                    'reply_allowed' => true,
+                    'product' => $product,
+                    'ajax_append' => true), 'product.html');
+                break;
+            
+            default:
+                $html = '';
+                break;
+        }
 
         echo $html;
         exit();
-    }  
+    }      
+
+    private function renderTemplate($assign, $template) {
+
+        $theme = waRequest::param('theme', 'default');
+        $theme_path = wa()->getDataPath('themes', true).'/'.$theme;
+
+        if (!file_exists($theme_path) || !file_exists($theme_path.'/theme.xml')) {
+            $theme_path = wa()->getAppPath().'/themes/'.$theme;
+        }
+
+        $view = wa()->getView(array('template_dir' => $theme_path));
+
+        // $view->assign($assign);
+
+        $template = $this->setThemeTemplate($template);
+        $result = $view->fetch($this->getTemplate());
+
+
+
+        return $result;
+
+    }
+
+    public function getTheme() {
+        $theme = new waTheme(waRequest::getTheme());
+
+        return $theme;
+    }
      
 }
