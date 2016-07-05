db9252505a1f57cba4f330664b840c5e512d4178
Max Severin <makc.severin@gmail.com>
2016-07-05 07:13:02 +0300

    design of the frontend template changed

--- lib/actions/shopFprviewPluginFrontendFprview.controller.php
+++ lib/actions/shopFprviewPluginFrontendFprview.controller.php
@@ -18,21 +18,39 @@ class shopFprviewPluginFrontendFprviewController extends shopFrontendAction {
 
         $feature_codes = array_keys($product->features);
         $feature_model = new shopFeatureModel();
-        $features = $feature_model->getByCode($feature_codes);
+        $features = $feature_model->getByCode($feature_codes);        
 
-        $view = wa()->getView();
-        $view->assign('features', $features);
-        $view->assign('product', $product);
+        $theme = waRequest::param('theme', 'default');
+        $theme_path = wa()->getDataPath('themes', true).'/'.$theme;
+
+        if (!file_exists($theme_path) || !file_exists($theme_path.'/theme.xml')) {
+            $theme_path = wa()->getAppPath().'/themes/'.$theme;
+        }
 
         switch ($settings['template_type']) {
             case 'plugin':
+
+                $view = wa()->getView();
+                $view->assign('features', $features);
+                $view->assign('product', $product);
+                $view->assign('theme_path', $theme_path);
+
                 $html = $view->fetch(realpath(dirname(__FILE__)."/../../").'/templates/Frontend.html');
+
                 break;
             case 'theme':
-                $html = $this->renderTemplate(array(
-                    'reply_allowed' => true,
-                    'product' => $product,
-                    'ajax_append' => true), 'product.html');
+
+                $view = wa()->getView(array('template_dir' => $theme_path));
+
+                $view->assign(array(
+                    'reply_allowed' => false,
+                    'features' => $features,
+                    'product' => $product
+                ));
+
+                $template = $this->setThemeTemplate('product.html');
+                $html = $view->fetch($this->getTemplate());
+
                 break;
             
             default:
@@ -43,33 +61,5 @@ class shopFprviewPluginFrontendFprviewController extends shopFrontendAction {
         echo $html;
         exit();
     }      
-
-    private function renderTemplate($assign, $template) {
-
-        $theme = waRequest::param('theme', 'default');
-        $theme_path = wa()->getDataPath('themes', true).'/'.$theme;
-
-        if (!file_exists($theme_path) || !file_exists($theme_path.'/theme.xml')) {
-            $theme_path = wa()->getAppPath().'/themes/'.$theme;
-        }
-
-        $view = wa()->getView(array('template_dir' => $theme_path));
-
-        // $view->assign($assign);
-
-        $template = $this->setThemeTemplate($template);
-        $result = $view->fetch($this->getTemplate());
-
-
-
-        return $result;
-
-    }
-
-    public function getTheme() {
-        $theme = new waTheme(waRequest::getTheme());
-
-        return $theme;
-    }
      
 }
