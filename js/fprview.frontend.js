7ede05d561f7caac5876f42790534b456f6729d3
Max Severin <makc.severin@gmail.com>
2016-08-07 22:54:24 +0300

    bug fix when finding button parent selector

--- js/fprview.frontend.js
+++ js/fprview.frontend.js
@@ -8,24 +8,24 @@
 //------------------- BEGIN JQUERY FUNCTIONS ------------------
 {if isset($fprview_settings.button_hide) && $fprview_settings.button_hide === 'on'}
 jQuery.fn.extend({
-    getSelectorPath: function () {
-        var path, node = this;
+	getSelectorPath: function () {
+		var path, node = this;
 
-        while (node.length) {
-            var realNode = node[0], name = realNode.localName;
+		while (node.length) {
+			var realNode = node[0], name = realNode.localName;
 
-            if (!name) break;
+			if (!name) break;
 
-            name = name.toLowerCase();
+			name = name.toLowerCase();
 
-            var parent = node.parent();
+			var parent = node.parent();
 
-            path = name + (path ? '>' + path : '');
-            node = parent;
-        }
+			path = name + (path ? '>' + path : '');
+			node = parent;
+		}
 
-        return path;
-    }
+		return path;
+	}
 });
 {/if}
 //-------------------- END JQUERY FUNCTIONS -------------------
@@ -42,36 +42,36 @@ var fprviewFrontend = (function () { "use strict";
 		$(document).on('click', '.fprview-more-info', function(event){
 			event.preventDefault();
 
-	        $.fancybox({
-	            type     : 'ajax',
-	            href     : $(this).attr('href'),
-	        	autoSize : false,
-			    width    : "60%",
-			    height   : "auto"
-	        });
+			$.fancybox({
+				type     : 'ajax',
+				href     : $(this).attr('href'),
+				autoSize : false,
+				width    : "60%",
+				height   : "auto"
+			});
 
-	        {if isset($fprview_settings.button_hide) && $fprview_settings.button_hide === 'on'}
-	        $(this).hide();
-	        {/if}
-	    });  
+			{if isset($fprview_settings.button_hide) && $fprview_settings.button_hide === 'on'}
+			$(this).hide();
+			{/if}
+		});  
 
 		{if isset($fprview_settings.button_hide) && $fprview_settings.button_hide === 'on'}
 		var viewButtonClass = '.fprview-more-info',
-	 		viewButtonParent = $(viewButtonClass).parent();
-console.log(viewButtonParent.getSelectorPath());
-	    $(document).on('mouseenter', viewButtonParent.getSelectorPath(), function () {
-	    	var viewButton = $(this).find(viewButtonClass);
-	    	if (viewButton) {
-	    		viewButton.css('display', 'block');
-	    	}
-	    });
-	    $(document).on('mouseleave', viewButtonParent.getSelectorPath(), function () {
-	    	var viewButton = $(this).find(viewButtonClass);
-	    	if (viewButton) {
-	    		viewButton.css('display', 'none');
-	    	}
-	    });
-	    {/if}
+		    viewButtonParent = $(viewButtonClass).parent();
+
+		$(document).on('mouseenter', viewButtonParent.getSelectorPath(), function () {
+			var viewButton = $(this).find(viewButtonClass);
+				if (viewButton) {
+				viewButton.css('display', 'block');
+			}
+		});
+		$(document).on('mouseleave', viewButtonParent.getSelectorPath(), function () {
+			var viewButton = $(this).find(viewButtonClass);
+				if (viewButton) {
+				viewButton.css('display', 'none');
+			}
+		});
+		{/if}
 
 	};
 
