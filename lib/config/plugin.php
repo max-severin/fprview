e7a88b49f6546e6344678401cfa77a66634fd3d7
Max Severin <makc.severin@gmail.com>
2016-08-14 18:17:18 +0300

    new version

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'description' => '',
     'img' => 'img/fprview.png',
     'vendor' => 1020720,
-    'version' => '1.1.4',
+    'version' => '1.2.0',
     'shop_settings' => true,
     'frontend' => true,
     'handlers' => array(
